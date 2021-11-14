1e1be77b33531685fe28b30ee4810170ed19f570
Alexis Meysson <alexis1722086@gmail.com>
2021-11-14 18:58:27 +0100

    Initial commit

---
+++ include/footer.php
@@ -0,0 +1,4 @@
+<footer class="fixed-bottom">
+    <h2 class="text-center">Copyright, realisé par Meysson Alexis</h2>
+    <a href="admin/connexion.php">Administrateur</a>
+</footer>
---
+++ include/footer2.php
@@ -0,0 +1,4 @@
+<footer class="fixed-bottom">
+    <h5 class="text-center">Copyright, realisé par Meysson Alexis</h5>
+    <a href="../logout.php">Déconnexion</a>
+</footer>
